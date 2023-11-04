9f898dbd46e5bc36823b28f6ba9c7ea33636a5c9
wllysses <wllyssestavares@gmail.com>
2023-11-04 08:18:30 -0300

    fix: export repo type

--- src/app/portfolio/components/project-card.tsx
+++ src/app/portfolio/components/project-card.tsx
@@ -8,15 +8,27 @@ import {
 } from "@/components/ui/card";
 import { Code2Icon, GitForkIcon, StarIcon } from "lucide-react";
 import Link from "next/link";
-import { Repo } from "../[slug]/page";
+import { Repo } from "@/types";
 
 interface ProjectCardProps extends Repo {}
 
-export function ProjectCard({ name, html_url, stargazers_count, forks_count, language, homepage }: ProjectCardProps) {
+export function ProjectCard({
+  name,
+  html_url,
+  stargazers_count,
+  forks_count,
+  language,
+  homepage,
+}: ProjectCardProps) {
   return (
     <Card className="hover:scale-105 hover:duration-100 hover:shadow-md hover:shadow-primary">
       <CardHeader>
-        <CardTitle className="whitespace-nowrap overflow-hidden text-ellipsis" title={name}>{name}</CardTitle>
+        <CardTitle
+          className="whitespace-nowrap overflow-hidden text-ellipsis"
+          title={name}
+        >
+          {name}
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <ul className="w-full flex items-center gap-4">
@@ -35,17 +47,17 @@ export function ProjectCard({ name, html_url, stargazers_count, forks_count, lan
         </ul>
 
         <div className="mt-4 flex items-center gap-2">
-          <Link 
-            href={html_url} 
-            target="_blank" 
+          <Link
+            href={html_url}
+            target="_blank"
             className={buttonVariants({ variant: "secondary" })}
           >
             Repositório
           </Link>
           {homepage && (
-            <Link 
-              href={homepage} 
-              target="_blank" 
+            <Link
+              href={homepage}
+              target="_blank"
               className={buttonVariants({ variant: "secondary" })}
             >
               Deploy
