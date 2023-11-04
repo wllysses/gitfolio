7aa3c91e97e28ccca490d95488108ee30c30dd1e
wllysses <wllyssestavares@gmail.com>
2023-11-04 08:20:14 -0300

    fix: export repo type on projects component

--- src/app/portfolio/components/projects.tsx
+++ src/app/portfolio/components/projects.tsx
@@ -1,28 +1,30 @@
 import { ProjectCard } from "./project-card";
-import { Repo } from "../[slug]/page";
+import { Repo } from "@/types";
 
 interface ProjectsProps {
-    data: Repo[]
+  data: Repo[];
 }
 
 export function Projects({ data }: ProjectsProps) {
-    return (
-        <section id="projects" className="mt-28 w-full p-2">
-            <h2 className="font-bold text-3xl text-center animate-fade-down">Meus <span className="text-primary">Projetos</span></h2>
-            <div className="my-8 grid grid-cols-4 gap-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 animate-fade-up">
-                {data && data.map((repo) => (
-                    <ProjectCard 
-                        key={repo.id}
-                        name={repo.name}
-                        html_url={repo.html_url}
-                        stargazers_count={repo.stargazers_count}
-                        forks_count={repo.forks_count}
-                        language={repo.language}
-                        homepage={repo.homepage}
-                    />
-
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section id="projects" className="mt-28 w-full p-2">
+      <h2 className="font-bold text-3xl text-center animate-fade-down">
+        Meus <span className="text-primary">Projetos</span>
+      </h2>
+      <div className="my-8 grid grid-cols-4 gap-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 animate-fade-up">
+        {data &&
+          data.map((repo) => (
+            <ProjectCard
+              key={repo.id}
+              name={repo.name}
+              html_url={repo.html_url}
+              stargazers_count={repo.stargazers_count}
+              forks_count={repo.forks_count}
+              language={repo.language}
+              homepage={repo.homepage}
+            />
+          ))}
+      </div>
+    </section>
+  );
 }
