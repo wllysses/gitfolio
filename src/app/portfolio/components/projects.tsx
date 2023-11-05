e734afe9b6c707be53fa82c518f06829cfc740a6
wllysses <wllyssestavares@gmail.com>
2023-11-05 09:52:50 -0300

    feat: repos loading using react query and load more repos button

--- src/app/portfolio/components/projects.tsx
+++ src/app/portfolio/components/projects.tsx
@@ -1,19 +1,43 @@
-import { ProjectCard } from "./project-card";
+"use client";
+
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { getUserRepos } from "@/services/api";
 import { Repo } from "@/types";
+import { ProjectCard } from "./project-card";
+import { Spinner } from "./spinner";
+import { Button } from "@/components/ui/button";
 
 interface ProjectsProps {
-  data: Repo[];
+  slug: string;
+  totalRepos: number;
 }
 
-export function Projects({ data }: ProjectsProps) {
+export function Projects({ slug, totalRepos }: ProjectsProps) {
+  const [perPage, setPerPage] = useState(8);
+
+  const {
+    data: repos,
+    isLoading,
+    isError,
+  } = useQuery<Repo[]>(["repos", perPage], async () =>
+    getUserRepos(slug, perPage)
+  );
+
+  if (isError) return <div>Algo deu errado...</div>;
+
   return (
-    <section id="projects" className="mt-28 w-full p-2">
+    <section
+      id="projects"
+      className="mt-28 mb-12 w-full p-2 flex flex-col gap-4 items-center"
+    >
       <h2 className="font-bold text-3xl text-center animate-fade-down">
         Meus <span className="text-primary">Projetos</span>
       </h2>
-      <div className="my-8 grid grid-cols-4 gap-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 animate-fade-up">
-        {data &&
-          data.map((repo) => (
+      {isLoading && <Spinner />}
+      <div className="w-full my-8 grid grid-cols-4 gap-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 animate-fade-up">
+        {repos &&
+          repos?.map((repo) => (
             <ProjectCard
               key={repo.id}
               name={repo.name}
@@ -25,6 +49,17 @@ export function Projects({ data }: ProjectsProps) {
             />
           ))}
       </div>
+      {totalRepos === 0 && <div>Nenhum repositório público.</div>}
+
+      {totalRepos > 8 && (
+        <Button
+          size="lg"
+          className="text-white"
+          onClick={() => setPerPage((prevState) => (prevState += 8))}
+        >
+          Carregar mais
+        </Button>
+      )}
     </section>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -5,9 +5,9 @@ export const getUserData = async (profileName: string) => {
   return await response.json();
 };
 
-export const getUserRepos = async (profileName: string) => {
+export const getUserRepos = async (profileName: string, perPage: number) => {
   const response = await fetch(
-    `https://api.github.com/users/${profileName}/repos`,
+    `https://api.github.com/users/${profileName}/repos?per_page=${perPage}`,
     { cache: "no-store" }
   );
   return await response.json();
