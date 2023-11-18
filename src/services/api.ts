44cf77a75fa9ceea845a8fd3b52773966f4b54b8
Vinicius Ribeiro <vayne99@gmail.com>
2023-11-18 17:51:49 +0000

    feat: inifinite loading on user projects

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,12 +6,18 @@ export const getUserData = async (profileName: string) => {
   return await response.json();
 };
 
-export const getUserRepos = async (profileName: string, perPage: number) => {
+export const getUserRepos = async (profileName: string, page: number) => {
   const response = await fetch(
-    `https://api.github.com/users/${profileName}/repos?per_page=${perPage}`,
-    { cache: "no-store" }
+    `https://api.github.com/users/${profileName}/repos?per_page=8&page=${page}`,
+    {
+      cache: "no-store",
+    }
   );
-  return await response.json();
+
+  const data = await response.json();
+  const nextPage = data.length === 0 ? null : page + 1;
+
+  return { data, nextPage };
 };
 
 export const getReposWithLanguages = async (profileName: string) => {
