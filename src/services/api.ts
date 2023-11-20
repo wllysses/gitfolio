ee970b509bc2df979fcc981cbcb4c90a9221adc1
Vinicius Ribeiro <vayne99@gmail.com>
2023-11-20 22:45:54 +0000

    feat: get custom pinned repositories from README

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,6 @@
+import { getTextBetween } from "@/lib/utils";
+import { Repo } from "@/types";
+
 export const getUserData = async (profileName: string) => {
   const response = await fetch(`https://api.github.com/users/${profileName}`, {
     cache: "no-store",
@@ -14,7 +17,7 @@ export const getUserRepos = async (profileName: string, page: number) => {
     }
   );
 
-  const data = await response.json();
+  const data = (await response.json()) as Repo[];
   const nextPage = data.length === 0 ? null : page + 1;
 
   return { data, nextPage };
@@ -26,4 +29,50 @@ export const getReposWithLanguages = async (profileName: string) => {
     { cache: "no-store" }
   );
   return await response.json();
+};
+
+export const getRepoREADME = async (githubAccount: string) => {
+  const response = await fetch(
+    `https://api.github.com/repos/${githubAccount}/gitfolio-custom/contents/README.md`,
+    { headers: { Accept: "application/vnd.github.raw" } }
+  );
+
+  if (!response.ok) return null;
+
+  const resultText = await response.text();
+
+  return resultText;
+};
+
+export const getREADMERepos = async (githubAccount: string) => {
+  const READMEText = await getRepoREADME(githubAccount);
+  if (!READMEText) return { data: [], nextPage: null };
+
+  const repos = getTextBetween(READMEText, "<pinned-repos>", "</pinned-repos>");
+
+  const reposArray = repos.trim().split(";");
+
+  const reposResponse = await Promise.allSettled(
+    reposArray.map(
+      async (repo) =>
+        await fetch(`https://api.github.com/repos/${githubAccount}/${repo}`, {
+          headers: {
+            Authorization:
+              "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",
+          },
+        })
+    )
+  );
+
+  const reposFound = reposResponse.filter(
+    (response) => response.status === "fulfilled"
+  );
+
+  const reposData: Repo[] = await Promise.all(
+    reposFound.map(
+      async (repo) => repo.status === "fulfilled" && repo.value.json()
+    )
+  );
+
+  return { data: reposData, nextPage: null };
 };
